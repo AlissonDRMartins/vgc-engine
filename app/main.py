196f05f713918b05773a0b53e61dbe127884b499
AlissonDRMartins <154258598+AlissonDRMartins@users.noreply.github.com>
2025-04-26 22:50:17 -0300

    feat: added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import endpoints
 
 app = FastAPI()
 
 app.include_router(endpoints.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],  # ou uma lista específica de domínios permitidos
+    allow_credentials=True,
+    allow_methods=[""],  # precisa permitir OPTIONS, POST, etc.
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
