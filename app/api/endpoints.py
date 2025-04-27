f4bcb8b7f057cb2a11dcfa1b283826325b02ee6b
AlissonDRMartins <154258598+AlissonDRMartins@users.noreply.github.com>
2025-04-26 21:21:03 -0300

    fix: spelling

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -32,5 +32,5 @@ def get_top_pokemons(highest_usage_pokemons: HighestUsagePokemons):
 @router.post("/moves_details")
 def get_move_details(moves_list: MovesList):
     client = SupabaseService(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_API_KEY"))
-    response = client.get_moves_details(moves_list)
+    response = client.get_moves_details(moves_list.moves)
     return {"moves": response}
